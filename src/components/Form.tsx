aac9940d2f11590ba5eacc6a03860739c5a8f357
phuong_tran <Phuong.Tran@primark.onmicrosoft.com>
2023-04-19 21:55:36 +0700

    fixed build

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -12,37 +12,38 @@ import {
   FormLabel,
   TextField
 } from '@mui/material';
-import { DatePicker } from '@mui/x-date-pickers';
 
 const API_GATEWAY_URL = 'https://4brve5hkh9.execute-api.us-east-1.amazonaws.com/stage6/resource';
 
-function getDataFromFile(file) {
-  return new Promise((resolve, reject) => {
+function getDataFromFile(file: File): Promise<string> {
+  return new Promise<string> ((resolve, reject) => {
     const reader = new FileReader();
 
-    reader.addEventListener("load", () => resolve(reader.result));
+    reader.addEventListener("load", () => resolve(reader.result as string));
     reader.addEventListener("error", err => reject(err));
     reader.readAsDataURL(file);
   });
 }
 
-
 const Form = () => {
   const { register, handleSubmit, reset } = useForm();
 
-  const onSubmit = async (data) => {
+  const onSubmit = async (data: FormData) => {
     try {
-      let fileData;
-      if (data.file.length > 0) {
-        fileData = await getDataFromFile(data.file[0]);
-      }
-      const response = await fetch(API_GATEWAY_URL, {
-        method: 'POST',
-        body: JSON.stringify({ ...data, file: fileData }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+      let fileData: string | undefined;
+			const file = data.get('file') as File;
+
+			if (file) {
+				fileData = await getDataFromFile(file);
+			}
+			const response = await fetch(API_GATEWAY_URL, {
+				method: 'POST',
+				body: JSON.stringify({ ...Object.fromEntries(data), file: fileData }),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+
       const responseData = await response.json();
       reset();
     } catch (error) {
@@ -53,6 +54,7 @@ const Form = () => {
   return (
     <Stack
       component="form"
+			// @ts-ignore
       onSubmit={handleSubmit(onSubmit)}
       py={2}
       px={3}
@@ -60,30 +62,27 @@ const Form = () => {
     >
       <TextField
         label="Title"
-        name="title"
         {...register('title')}
         fullWidth
       />
 
       <TextField
         label="Description"
-        name="description"
         {...register('description')}
         fullWidth
         multiline
         rows={4}
       />
 
-      <DatePicker
+			<TextField
         label="Date"
-        name="date"
+        type="date"
         {...register('date')}
         fullWidth
       />
 
       <TextField
         label="File"
-        name="file"
         type="file"
         {...register('file')}
         fullWidth
